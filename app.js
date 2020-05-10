e6d883aae4ba4e1c7ab683fa77e09ec1d653e5e8
Etoka Kingsley <etokakingsley@gmail.com>
2020-05-10 21:57:02 +0100

    test-counter-app

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const path = require('path')
 const helmet = require('helmet')
 
 const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use(helmet())
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/', (req, res, next)=>{
-    res.send('index.html')
+app.get('/', (req, res, next)=>{
+    res.sendFile('./index.html')
 })
 
-app.listen(process.env.PORT, ()=>console.log(process.env.PORT));
+let count = 0;
+
+io.on('connection',socket=>{
+    console.log(socket)
+    console.log('new connection ')
+    socket.emit('countUpdated',count);
+    socket.on('increment',()=>{
+        count++
+        //using socket.emit sends only to a particular client which is the connected one
+        //socket.emit('countUpdated',count);
+        io.emit('countUpdated',count);
+    })
+});
+
+http.listen(process.env.PORT, ()=>console.log(process.env.PORT));
